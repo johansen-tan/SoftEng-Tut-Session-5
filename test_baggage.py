2665a315988d5e30df397ae7f96222d85f29d03e
Johansen Tan <johansen.tan@binus.ac.id>
2026-05-23 15:32:14 +0700

    test: add failing test for hazardous items

--- test_baggage.py
+++ test_baggage.py
@@ -1,2 +1,9 @@
 import pytest
 from src.baggage import validate_baggage 
+
+_FAILED_MESSAGES = {
+    "hazardous": "Hazardous items detected. Can't proceed further.",
+}
+
+def test_hazardous_item_prohibited():
+    assert validate_baggage(5, "carry-on", "economy", "domestic", hazardous_item=True) == _FAILED_MESSAGES["hazardous"]
