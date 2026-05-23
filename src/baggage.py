324c3a6ffbb0f4af795ea154fe40c233e422892c
theodore.alexander001@binus.ac.id <theodore.alexander001@binus.ac.id>
2026-05-23 15:16:29 +0700

    menambahkan success message dan function test flight

--- src/baggage.py
+++ src/baggage.py
@@ -1,2 +1,5 @@
 def validate_baggage():
     return -1
+_SUCCESS_MESSAGES = {"domestic": "Checked in successful.", 
+                     "international": "Checked in successful. Passport is required"}
+
--- test_baggage.py
+++ test_baggage.py
@@ -1,2 +1,8 @@
 import pytest
 from src.baggage import validate_baggage 
+
+def test_flight_type():
+    assert validate_baggage(5, "carry-on", "economy", "domestic", hazardous_item=False) == _SUCCESS_MESSAGES["domestic"]
+    assert validate_baggage(25, "checked", "economy", "domestic", hazardous_item=False) == _SUCCESS_MESSAGES["domestic"]
+    assert validate_baggage(7, "carry-on", "economy", "international", hazardous_item=False) == _SUCCESS_MESSAGES["international"]
+    assert validate_baggage(30, "checked", "economy", "international", hazardous_item=False) == _SUCCESS_MESSAGES["international"]
